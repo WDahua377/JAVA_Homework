bae42b3c5c93e095663a7eeebfa40edb9d763dfd
WDahua377 <danmei891120@gmail.com>
2025-09-22 19:33:28 +0800

    WIP: refactor addKey to support multiple characters in one button

--- HW4/TypingTutor.java
+++ HW4/TypingTutor.java
@@ -10,6 +10,7 @@ import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.security.KeyException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,72 +18,27 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 
-public class TypingTutor extends JFrame implements KeyListener{
+import java.util.HashMap;
+import java.util.Map;
 
-    public static JButton tilde;
-    public static JButton one;
-    public static JButton two;
-    public static JButton three;
-    public static JButton four;
-    public static JButton five;
-    public static JButton six;
-    public static JButton seven;
-    public static JButton eight;
-    public static JButton nine;
-    public static JButton zero;
-    public static JButton dash;
-    public static JButton plus;
-    public static JButton backspace;
-    public static JButton tab;
-    public static JButton Q;
-    public static JButton W;
-    public static JButton E;
-    public static JButton R;
-    public static JButton T;
-    public static JButton Y;
-    public static JButton U;
-    public static JButton I;
-    public static JButton O;
-    public static JButton P;
-    public static JButton lbracket;
-    public static JButton rbracket;
-    public static JButton backslash;
-    public static JButton caps;
-    public static JButton A;
-    public static JButton S;
-    public static JButton D;
-    public static JButton F;
-    public static JButton G;
-    public static JButton H;
-    public static JButton J;
-    public static JButton K;
-    public static JButton L;
-    public static JButton colon;
-    public static JButton quote;
-    public static JButton enter;
-    public static JButton shift;
-    public static JButton Z;
-    public static JButton X;
-    public static JButton C;
-    public static JButton V;
-    public static JButton B;
-    public static JButton N;
-    public static JButton M;
-    public static JButton comma;
-    public static JButton dot;
-    public static JButton question;
-    public static JButton up;
-    public static JButton down;
-    public static JButton left;
-    public static JButton right;
-    public static JButton space;
+public class TypingTutor_ver2 extends JFrame implements KeyListener{
+
+    // public static JButton tilde, one, two, three, four, five, six, seven, eight, nine, zero, dash, plus, backspace;
+    // public static JButton tab, Q, W, E, R, T, Y, U, I, O, P, lbracket, rbracket, backslash;
+    // public static JButton caps, A, S, D, F, G, H, J, K, L, colon, quote, enter;
+    // public static JButton shift, Z, X, C, V, B, N, M, comma, dot, question;
+    // public static JButton up, down, left, right, space;
+    // public static JButton backspace, tab, caps, enter, shift;
+    public static Map<Character, JButton> keyMap;
+    public static Map<Integer, JButton> speicalKeyMap;
     public static JLabel label;
     public static JTextArea textarea;
 
-    public TypingTutor(){
+    public TypingTutor_ver2(){
 
         super("Typing Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(820, 550);
 
         JPanel upPanel = new JPanel();
         JPanel keyPanel = new JPanel();
@@ -103,210 +59,105 @@ public class TypingTutor extends JFrame implements KeyListener{
         upPanel.add(textarea);
         
         keyPanel.setPreferredSize(new Dimension(820, 300));
-        tilde = new JButton("~");
-        tilde.setBounds(35, 0, 45, 45);
-        keyPanel.add(tilde);
-        one = new JButton("1");
-        one.setBounds(85, 0, 45, 45);
-        keyPanel.add(one);
-        two = new JButton("2");
-        two.setBounds(135, 0, 45, 45);
-        keyPanel.add(two);
-        three = new JButton("3");
-        three.setBounds(185, 0, 45, 45);
-        keyPanel.add(three);
-        four = new JButton("4");
-        four.setBounds(235, 0, 45, 45);
-        keyPanel.add(four);
-        five = new JButton("5");
-        five.setBounds(285, 0, 45, 45);
-        keyPanel.add(five);
-        six = new JButton("6");
-        six.setBounds(335, 0, 45, 45);
-        keyPanel.add(six);
-        seven = new JButton("7");
-        seven.setBounds(385, 0, 45, 45);
-        keyPanel.add(seven);
-        eight = new JButton("8");
-        eight.setBounds(435, 0, 45, 45);
-        keyPanel.add(eight);
-        nine = new JButton("9");
-        nine.setBounds(485, 0, 45, 45);
-        keyPanel.add(nine);
-        zero = new JButton("0");
-        zero.setBounds(535, 0, 45, 45);
-        keyPanel.add(zero);
-        dash = new JButton("-");
-        dash.setBounds(585, 0, 45, 45);
-        keyPanel.add(dash);
-        plus = new JButton("+");
-        plus.setBounds(635, 0, 45, 45);
-        keyPanel.add(plus);
-        backspace = new JButton("Baskspace");
-        backspace.setBounds(685, 0, 100, 45);
-        keyPanel.add(backspace);
-        tab = new JButton("Tab");
-        tab.setBounds(35, 50, 70, 45);
-        keyPanel.add(tab);
-        Q = new JButton("Q");
-        Q.setBounds(110, 50, 45, 45);
-        keyPanel.add(Q);
-        W = new JButton("W");
-        W.setBounds(160, 50, 45, 45);
-        keyPanel.add(W);
-        E = new JButton("E");
-        E.setBounds(210, 50, 45, 45);
-        keyPanel.add(E);
-        R = new JButton("R");
-        R.setBounds(260, 50, 45, 45);
-        keyPanel.add(R);
-        T = new JButton("T");
-        T.setBounds(310, 50, 45, 45);
-        keyPanel.add(T);
-        Y = new JButton("Y");
-        Y.setBounds(360, 50, 45, 45);
-        keyPanel.add(Y);
-        U = new JButton("U");
-        U.setBounds(410, 50, 45, 45);
-        keyPanel.add(U);
-        I = new JButton("I");
-        I.setBounds(460, 50, 45, 45);
-        keyPanel.add(I);
-        O = new JButton("O");
-        O.setBounds(510, 50, 45, 45);
-        keyPanel.add(O);
-        P = new JButton("P");
-        P.setBounds(560, 50, 45, 45);
-        keyPanel.add(P);
-        lbracket = new JButton("[");
-        lbracket.setBounds(610, 50, 45, 45);
-        keyPanel.add(lbracket);
-        rbracket = new JButton("]");
-        rbracket.setBounds(660, 50, 45, 45);
-        keyPanel.add(rbracket);
-        backslash = new JButton("\\");
-        backslash.setBounds(710, 50, 45, 45);
-        keyPanel.add(backslash);
-        caps = new JButton("Caps");
-        caps.setBounds(35, 100, 70, 45);
-        keyPanel.add(caps);
-        A = new JButton("A");
-        A.setBounds(110, 100, 45, 45);
-        keyPanel.add(A);
-        S = new JButton("S");
-        S.setBounds(160, 100, 45, 45);
-        keyPanel.add(S);
-        D = new JButton("D");
-        D.setBounds(210, 100, 45, 45);
-        keyPanel.add(D);
-        F = new JButton("F");
-        F.setBounds(260, 100, 45, 45);
-        keyPanel.add(F);
-        G = new JButton("G");
-        G.setBounds(310, 100, 45, 45);
-        keyPanel.add(G);
-        H = new JButton("H");
-        H.setBounds(360, 100, 45, 45);
-        keyPanel.add(H);
-        J = new JButton("J");
-        J.setBounds(410, 100, 45, 45);
-        keyPanel.add(J);
-        K = new JButton("K");
-        K.setBounds(460, 100, 45, 45);
-        keyPanel.add(K);
-        L = new JButton("L");
-        L.setBounds(510, 100, 45, 45);
-        keyPanel.add(L);
-        colon = new JButton(":");
-        colon.setBounds(560, 100, 45, 45);
-        keyPanel.add(colon);
-        quote = new JButton("\"");
-        quote.setBounds(610, 100, 45, 45);
-        keyPanel.add(quote);
-        enter = new JButton("Enter");
-        enter.setBounds(660, 100, 95, 45);
-        keyPanel.add(enter);
-        shift = new JButton("Shift");
-        shift.setBounds(35, 150, 95, 45);
-        keyPanel.add(shift);
-        Z = new JButton("Z");
-        Z.setBounds(135, 150, 45, 45);
-        keyPanel.add(Z);
-        X = new JButton("X");
-        X.setBounds(185, 150, 45, 45);
-        keyPanel.add(X);
-        C = new JButton("C");
-        C.setBounds(235, 150, 45, 45);
-        keyPanel.add(C);
-        V = new JButton("V");
-        V.setBounds(285, 150, 45, 45);
-        keyPanel.add(V);
-        B = new JButton("B");
-        B.setBounds(335, 150, 45, 45);
-        keyPanel.add(B);
-        N = new JButton("N");
-        N.setBounds(385, 150, 45, 45);
-        keyPanel.add(N);
-        M = new JButton("M");
-        M.setBounds(435, 150, 45, 45);
-        keyPanel.add(M);
-        comma = new JButton(",");
-        comma.setBounds(485, 150, 45, 45);
-        keyPanel.add(comma);
-        dot = new JButton(",");
-        dot.setBounds(535, 150, 45, 45);
-        keyPanel.add(dot);
-        question = new JButton("?");
-        question.setBounds(585, 150, 45, 45);
-        keyPanel.add(question);
-        up = new JButton("^");
-        up.setBounds(660, 150, 45, 45);
-        keyPanel.add(up);
-        space = new JButton();
-        space.setBounds(230, 200, 305, 45);
-        keyPanel.add(space);
-        left = new JButton("<");
-        left.setBounds(610, 200, 45, 45);
-        keyPanel.add(left);
-        down = new JButton("v");
-        down.setBounds(660, 200, 45, 45);
-        keyPanel.add(down);
-        right = new JButton(">");
-        right.setBounds(710, 200, 45, 45);
-        keyPanel.add(right);
-
+        keyMap = new HashMap<>();
+        addKey(keyMap, keyPanel, "~", 35, 0, 45, 45);
+        // keyMap.put('~', new JButton ("~"));
+        // keyMap.get('~').setBounds(35, 0, 45, 45);
+        // keyPanel.add(keyMap.get('~'));
+        addKey(keyMap, keyPanel, "1", 85, 0, 45, 45);
+        addKey(keyMap, keyPanel, "2", 135, 0, 45, 45);
+        addKey(keyMap, keyPanel, "3", 185, 0, 45, 45);
+        addKey(keyMap, keyPanel, "4", 235, 0, 45, 45);
+        addKey(keyMap, keyPanel, "5", 285, 0, 45, 45);
+        addKey(keyMap, keyPanel, "6", 335, 0, 45, 45);
+        addKey(keyMap, keyPanel, "7", 385, 0, 45, 45);
+        addKey(keyMap, keyPanel, "8", 435, 0, 45, 45);
+        addKey(keyMap, keyPanel, "9", 485, 0, 45, 45);
+        addKey(keyMap, keyPanel, "0", 535, 0, 45, 45);
+        addKey(keyMap, keyPanel, "-", 585, 0, 45, 45);
+        addKey(keyMap, keyPanel, "+", 635, 0, 45, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_BACK_SPACE, "Backspace", 685, 0, 100, 45);
+        // backspace = new JButton("Backspace");
+        // backspace.setBounds(685, 0, 100, 45);
+        // keyPanel.add(backspace);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_TAB, "Tab", 35, 50, 70, 45);
+        addKey(keyMap, keyPanel, "Q", 110, 50, 45, 45);
+        addKey(keyMap, keyPanel, "W", 160, 50, 45, 45);
+        addKey(keyMap, keyPanel, "E", 210, 50, 45, 45);
+        addKey(keyMap, keyPanel, "R", 260, 50, 45, 45);
+        addKey(keyMap, keyPanel, "T", 310, 50, 45, 45);
+        addKey(keyMap, keyPanel, "Y", 360, 50, 45, 45);
+        addKey(keyMap, keyPanel, "U", 410, 50, 45, 45);
+        addKey(keyMap, keyPanel, "I", 460, 50, 45, 45);
+        addKey(keyMap, keyPanel, "O", 510, 50, 45, 45);
+        addKey(keyMap, keyPanel, "P", 560, 50, 45, 45);
+        addKey(keyMap, keyPanel, "[", 610, 50, 45, 45);
+        addKey(keyMap, keyPanel, "]", 660, 50, 45, 45);
+        addKey(keyMap, keyPanel, "\\", 710, 50, 45, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_CAPS_LOCK, "Caps", 35, 100, 70, 45);
+        addKey(keyMap, keyPanel, "A", 110, 100, 45, 45);
+        addKey(keyMap, keyPanel, "S", 160, 100, 45, 45);
+        addKey(keyMap, keyPanel, "D", 210, 100, 45, 45);
+        addKey(keyMap, keyPanel, "F", 260, 100, 45, 45);
+        addKey(keyMap, keyPanel, "G", 310, 100, 45, 45);
+        addKey(keyMap, keyPanel, "H", 360, 100, 45, 45);
+        addKey(keyMap, keyPanel, "J", 410, 100, 45, 45);
+        addKey(keyMap, keyPanel, "K", 460, 100, 45, 45);
+        addKey(keyMap, keyPanel, "L", 510, 100, 45, 45);
+        addKey(keyMap, keyPanel, ":", 560, 100, 45, 45);
+        addKey(keyMap, keyPanel, "\"", 610, 100, 45, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_ENTER, "Enter", 660, 100, 95, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_SHIFT, "Shift", 35, 150, 45, 45);
+        addKey(keyMap, keyPanel, "Z", 135, 150, 45, 45);
+        addKey(keyMap, keyPanel, "X", 185, 150, 45, 45);
+        addKey(keyMap, keyPanel, "C", 235, 150, 45, 45);
+        addKey(keyMap, keyPanel, "V", 285, 150, 45, 45);
+        addKey(keyMap, keyPanel, "B", 335, 150, 45, 45);
+        addKey(keyMap, keyPanel, "N", 385, 150, 45, 45);
+        addKey(keyMap, keyPanel, "M", 435, 150, 45, 45);
+        addKey(keyMap, keyPanel, ",", 485, 150, 45, 45);
+        addKey(keyMap, keyPanel, ".", 535, 150, 45, 45);
+        addKey(keyMap, keyPanel, "?", 585, 150, 45, 45);
+        addKey(keyMap, keyPanel, " ", 230, 200, 305, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_UP, "^", 660, 150, 45, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_LEFT, "<", 610, 200, 45, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_DOWN, "v", 660, 200, 45, 45);
+        addKey(speicalKeyMap, keyPanel, KeyEvent.VK_RIGHT, ">", 710, 200, 45, 45);
 
         textarea.addKeyListener(this);
-        setSize(820, 550);
-        // pack();
+    }
 
+    public void addKey(Map<Character, JButton> map ,JPanel panel,char[] chars, String label, int x, int y, int w, int h){
+        char c = label.charAt(0);
+        map.put(c, new JButton(label));
+        map.get(label).setBounds(x, y, w, h);
+        panel.add(map.get(label));
+    }
+
+    public void addKey(Map<Integer, JButton> map, JPanel panel, int n, String label, int x, int y, int w, int h){
+        map.put(n, new JButton(label));
+        map.get(n).setBounds(x, y, w, h);
+        panel.add(map.get(n));
     }
 
     // function key
     public void keyPressed(KeyEvent event){
-        switch(event.getKeyCode()){
-            case KeyEvent.VK_CAPS_LOCK:
-                caps.setBackground(Color.GRAY);
-                break;
-            case KeyEvent.VK_UP:
-                up.setBackground(Color.GRAY);
-                break;
-            case KeyEvent.VK_DOWN:
-                down.setBackground(Color.GRAY);
-                break;
-            case KeyEvent.VK_LEFT:
-                left.setBackground(Color.GRAY);
-                break;
-            case KeyEvent.VK_RIGHT:
-                right.setBackground(Color.GRAY);
-                break;
-            case KeyEvent.VK_SHIFT:
-                shift.setBackground(Color.GRAY);
-                break;
+        JButton btn = speicalKeyMap.get(event.getKeyCode());
+        if(btn != null){
+            btn.setBackground(Color.GRAY);
         }
     }
 
     public void keyReleased(KeyEvent event){
+        JButton btn = null;
+        if(event.getKeyChar() != KeyEvent.CHAR_UNDEFINED){
+            btn = keyMap.get(event.getKeyChar());
+        }else{
+            btn = speicalKeyMap.get(event.getKeyCode());
+        }
+        btn.setBackground(textarea.getBackground());
+
+
         switch(event.getKeyChar()){
             case '~':
             case '`':
